d554ccc7576b0ec5bf2c7528d8cbdc9961e4997e
kareddibalaganesh <kareddibalaganesh@gmail.com>
2025-08-14 18:56:28 +0530

    aunty is updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,3 +3,9 @@ public class HelloWorld {
     System.out.println("Hello world!");
   }
 }
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello aunty!");
+  }
+}
